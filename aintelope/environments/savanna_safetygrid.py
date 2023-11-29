Fixing n_observations calculation in experiments.py. Convert observation to np.float32 dtype in gridworlds Zoo wrapper. Add code to select environment class based on configuration.

aintelope/environments/savanna_safetygrid.py
@@ -148,7 +148,9 @@ class GridworldZooBaseEnv:
         for x in info[INFO_OBSERVATION_COORDINATES].get(DRINK_CHR, []):
             agent_observations += list(x)  # convert tuple to list
 
-        agent_observations = np.array(agent_observations)
+        agent_observations = np.array(
+            agent_observations, np.float32
+        )  # NB! Q-agent expects float32 observation type
 
         assert (
             agent_observations.shape == self.observation_space(agent).shape
@@ -283,7 +285,9 @@ class SavannaGridworldParallelEnv(GridworldZooBaseEnv, GridworldZooParallelEnv):
     #        observations2[agent] = self.transform_observation(agent, infos[agent])
     #    return observations2, infos
 
-    def reset(self, seed: Optional[int] = None, options=None) -> Tuple[Dict[AgentId, Observation], Dict[AgentId, Info]]:
+    def reset(
+        self, seed: Optional[int] = None, options=None
+    ) -> Tuple[Dict[AgentId, Observation], Dict[AgentId, Info]]:
         observations, infos = GridworldZooParallelEnv.reset(self)
         self._last_infos = infos
         # transform observations
@@ -291,7 +295,7 @@ class SavannaGridworldParallelEnv(GridworldZooBaseEnv, GridworldZooParallelEnv):
             self.observations2[agent] = self.transform_observation(agent, infos[agent])
         return self.observations2, infos
 
-    def observe(self, agent = None) -> Union[Dict[AgentId, Observation], Observation]:
+    def observe(self, agent=None) -> Union[Dict[AgentId, Observation], Observation]:
         if agent is None:
             return self.observations2
         else:
@@ -364,7 +368,9 @@ class SavannaGridworldSequentialEnv(GridworldZooBaseEnv, GridworldZooAecEnv):
     #    # self._last_infos = infos
     #    return observations2, infos
 
-    def reset(self, seed: Optional[int] = None, options=None) -> Tuple[Dict[AgentId, Observation], Dict[AgentId, Info]]:
+    def reset(
+        self, seed: Optional[int] = None, options=None
+    ) -> Tuple[Dict[AgentId, Observation], Dict[AgentId, Info]]:
         GridworldZooAecEnv.reset(self)
 
         # observe observations, transform observations
@@ -377,7 +383,7 @@ class SavannaGridworldSequentialEnv(GridworldZooBaseEnv, GridworldZooAecEnv):
         self._last_infos = infos
         return self.observations2, infos
 
-    def observe(self, agent = None) -> Union[Dict[AgentId, Observation], Observation]:
+    def observe(self, agent=None) -> Union[Dict[AgentId, Observation], Observation]:
         if agent is None:
             return self.observations2
         else:
@@ -414,9 +420,7 @@ class SavannaGridworldSequentialEnv(GridworldZooBaseEnv, GridworldZooAecEnv):
 
         self._last_infos[agent] = info
 
-        logger.debug(
-            "debug return", observation2, reward2, terminated, truncated, info
-        )
+        logger.debug("debug return", observation2, reward2, terminated, truncated, info)
         return observation2, reward2, terminated, truncated, info
 
     def step_multiple_agents(self, actions: Dict[str, Action]) -> Step:

aintelope/training/dqn_training.py
@@ -65,19 +65,21 @@ class Trainer:
         # tb-logging and device control, check lightning_Trainer for 'AVAIL'
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.optimizer = optim.AdamW(
-            DQN(self.n_observations, self.action_space("agent_0").n).parameters(),    # TODO: multi-agent handling
+            DQN(
+                self.n_observations, self.action_space("agent_0").n
+            ).parameters(),  # TODO: multi-agent handling
             lr=self.hparams.lr,
             amsgrad=True,
         )  # refactor, making a dummy network now. problem is add_agent inits first real network---v
 
     def add_agent(self, agent_id):
         self.replay_memories[agent_id] = ReplayMemory(self.hparams.replay_size)
-        self.policy_nets[agent_id] = DQN(self.n_observations, self.action_space(agent_id).n).to(
-            self.device
-        )
-        self.target_nets[agent_id] = DQN(self.n_observations, self.action_space(agent_id).n).to(
-            self.device
-        )
+        self.policy_nets[agent_id] = DQN(
+            self.n_observations, self.action_space(agent_id).n
+        ).to(self.device)
+        self.target_nets[agent_id] = DQN(
+            self.n_observations, self.action_space(agent_id).n
+        ).to(self.device)
         self.target_nets[agent_id].load_state_dict(
             self.policy_nets[agent_id].state_dict()
         )