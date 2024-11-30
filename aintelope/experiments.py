9390c554aa50ecd2bd9fcfd02013c8acecb4f10b
Roland Pihlakas <roland@simplify.ee>
2024-11-30 15:05:24 +0200

    Removing multi-agent loop from SB3 training method.

--- aintelope/experiments.py
+++ aintelope/experiments.py
@@ -508,18 +508,10 @@ def run_baseline_training(cfg: DictConfig):
             )
         )
 
-    # Train
-    # for i_episode in range(cfg.hparams.num_episodes):
-    #    seed = i_episode
-    #    # env = get_env_class(cfg.hparams.env)(env_params=cfg.hparams.env_params)
-    #    # TODO: If the environments are cloned then this reset does nothing. And because of that there is also a question should we then have the episodes loop at all.
-    #    env.reset(trial_no=seed)    # TODO: trial_no in case multiple episodes use SAME seed
-
-    #    # for agent in agents:  # TODO: I do not think we can train the agents separately. When agents share the environment, they can only be trained at the same time. Otherwise it would be trained in a situation where only one agent moves and the other agent is always at the same location.
-    #    # agent.set_env(env)
-    #    # WORKS ONLY FOR ONE AGENT ATM! Cutoff here to synchronize the run
-    #    agents[0].train(cfg.hparams.env_params.num_iters)
-    agents[0].train(cfg.hparams.env_params.num_iters * cfg.hparams.num_episodes)
+    # SB3 models are designed for single-agent settings, we get around this by using he same model for every agent
+    # https://pettingzoo.farama.org/tutorials/sb3/waterworld/
+    # TODO: we could still allow multiple agents training if we use an appropriate env wrapper
+    agents[0].train(cfg.hparams.env_params.num_iters * 1)  # cfg.hparams.num_episodes)
 
     # Save models
     for agent in agents:
